a7b7c52518a1161d6110a2320dc64b51f2630510
Sharon-B <sharonbagnall@gmail.com>
2021-01-06 18:52:04 +0000

    Fix: capitalize constructor function names

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,12 @@
 // Create a Card object
-function card(value, name, suit) {
+function Card(value, name, suit) {
 	this.value = value;
 	this.name = name;
     this.suit = suit;
 };
 
 // Create a deck of cards
-function deck() {
+function Deck() {
 	this.names = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
     this.suits = ["♠", "♣", "♥", "♦"];
     
@@ -14,7 +14,7 @@ function deck() {
     
     for (var s = 0; s < this.suits.length; s++ ) {
         for (var n = 0; n < this.names.length; n++ ) {
-            cards.push(new card(n+1, this.names[n], this.suits[s]));
+            cards.push(new Card(n+1, this.names[n], this.suits[s]));
         }
     }
 
@@ -22,7 +22,7 @@ function deck() {
 };
 
 // Create a new deck of cards
-let myDeck = new deck();
+let myDeck = new Deck();
 
 console.log(myDeck);
 
