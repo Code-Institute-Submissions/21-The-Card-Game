bdf45b9b18fb5720d380d85b63d0c6274981a162
Sharon-B <sharonbagnall@gmail.com>
2021-01-06 18:33:24 +0000

    Create card and deck functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,30 @@
 // Create a Card object
+function card(value, name, suit) {
+	this.value = value;
+	this.name = name;
+    this.suit = suit;
+};
 
 // Create a deck of cards
+function deck() {
+	this.names = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+    this.suits = ["♠", "♣", "♥", "♦"];
+    
+	var cards = [];
+    
+    for (var s = 0; s < this.suits.length; s++ ) {
+        for (var n = 0; n < this.names.length; n++ ) {
+            cards.push(new card(n+1, this.names[n], this.suits[s]));
+        }
+    }
+
+    return cards;
+};
+
+// Create a new deck of cards
+let myDeck = new deck();
+
+console.log(myDeck);
 
 // Shuffle Cards
 
