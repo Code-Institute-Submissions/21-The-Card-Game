e9bd87b05875a4651ea3131d8c3f06fcdb992dfd
Sharon-B <sharonbagnall@gmail.com>
2021-01-06 17:57:42 +0000

    Add comments to game.js outlining game play/structure

---
+++ assets/js/game.js
@@ -0,0 +1,27 @@
+// Create a Card object
+
+// Create a deck of cards
+
+// Shuffle Cards
+
+// Deal cards to dealer
+
+// Deal cards to player
+
+// Assign & Add card values
+
+// Check Score
+
+// Start Game
+
+// Hit
+
+// Hold
+
+// Dealer Hit
+
+// Dealer Hold??
+
+// Game outcome? Win, Lose, Draw
+
+// Reset Game
