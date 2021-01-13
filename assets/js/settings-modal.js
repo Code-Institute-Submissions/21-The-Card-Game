6461efbfee0090bd0eac8d52da9b5f3df7bf6e5d
Sharon-B <sharonbagnall@gmail.com>
2021-01-13 14:33:58 +0000

    Update theme functions to change color of text elements

--- assets/js/settings-modal.js
+++ assets/js/settings-modal.js
@@ -21,19 +21,40 @@ let classicTheme = document.getElementById("classic");
 classicTheme.addEventListener("click", classic);
 function classic() {
     document.getElementById("background").className = "bg-color-classic";
-    document.getElementById("logo").className = "logo-classic";
+    document.getElementById("logo").className = "classic-theme";
+    document.getElementById("dealer-heading").className = "classic-theme";
+    document.getElementById("player-heading").className = "classic-theme";
+
+    document.getElementById("dealer-card-total").className += " classic-theme";
+    document.getElementById("dealer-card-total").classList.remove("night-theme");
+    document.getElementById("player-card-total").className += " classic-theme";
+    document.getElementById("player-card-total").classList.remove("night-theme");
 };
 
 let beachTheme = document.getElementById("beach");
 beachTheme.addEventListener("click", beach);
 function beach() {
     document.getElementById("background").className = "bg-color-beach";
-    document.getElementById("logo").className = "logo-classic";
+    document.getElementById("logo").className = "classic-theme";
+    document.getElementById("dealer-heading").className = "classic-theme";
+    document.getElementById("player-heading").className = "classic-theme";
+
+    document.getElementById("dealer-card-total").className += " classic-theme";
+    document.getElementById("dealer-card-total").classList.remove("night-theme");
+    document.getElementById("player-card-total").className += " classic-theme";
+    document.getElementById("player-card-total").classList.remove("night-theme");
 };
 
 let nightTheme = document.getElementById("night");
 nightTheme.addEventListener("click", night);
 function night() {
     document.getElementById("background").className = "bg-color-night";
-    document.getElementById("logo").className = "logo-night-theme"
+    document.getElementById("logo").className = "night-theme"
+    document.getElementById("dealer-heading").className = "night-theme";
+    document.getElementById("player-heading").className = "night-theme";
+    document.getElementById("dealer-card-total").className += " night-theme";
+    document.getElementById("dealer-card-total").classList.remove("classic-theme");
+    document.getElementById("player-card-total").className += " night-theme";
+    document.getElementById("player-card-total").classList.remove("classic-theme");
 };
+
